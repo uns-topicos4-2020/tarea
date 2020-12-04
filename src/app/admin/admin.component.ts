2c08197f16e9f39185960116ad4a65c7324bc50b
carlosVM <carlos_vm_7@hotmail.com>
2020-12-04 16:10:14 -0500

    endpoints

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,13 +2,44 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { UsersService } from '../services/users.service'
 
+
+class UserModel implements UserModel {
+  "id": any;
+  "createdAt": String;
+  "name": String;
+  "avatar": String;
+  "role": {
+      "__request_id": any,
+      "Transaction": [],
+      "Coupon": [],
+      "user": String,
+      "passsword": String
+  }
+
+}
+
+interface UserModel {
+"id": any;
+"createdAt": String;
+"name": String;
+"avatar": String;
+"role": {
+    "__request_id": any,
+    "Transaction": [],
+    "Coupon": [],
+    "user": String,
+    "passsword": String
+}
+
+}
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit, OnDestroy {
-
+  Users: Array<UserModel> = Array<UserModel>();
   constructor(private _UsersService: UsersService) { }
 
   ngOnInit() {
