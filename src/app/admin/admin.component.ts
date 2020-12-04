a1712a1d6db7f7a55926043e0ba5bdc13ec80880
carlosVM <carlos_vm_7@hotmail.com>
2020-12-04 15:45:46 -0500

    crud admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { UsersService } from '../services/users.service'
 
+declare const $:any;
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -9,9 +10,37 @@ import { UsersService } from '../services/users.service'
 })
 export class AdminComponent implements OnInit, OnDestroy {
 
+
+
+
   constructor(private _UsersService: UsersService) { }
 
   ngOnInit() {
+
+    $(document).ready(function(){
+      // Activate tooltip
+      $('[data-toggle="tooltip"]').tooltip();
+      
+      // Select/Deselect checkboxes
+      var checkbox = $('table tbody input[type="checkbox"]');
+      $("#selectAll").click(function(){
+        if(this.checked){
+          checkbox.each(function(){
+            this.checked = true;                        
+          });
+        } else{
+          checkbox.each(function(){
+            this.checked = false;                        
+          });
+        } 
+      });
+      checkbox.click(function(){
+        if(!this.checked){
+          $("#selectAll").prop("checked", false);
+        }
+      });
+    });
+
     this._UsersService.Users().subscribe(ok => {
       console.log(ok)
     }, err => {
