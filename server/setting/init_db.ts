4f4558f0266236913dfa7da4d3bb6c32ec98366b
carlosVM <carlos_vm_7@hotmail.com>
2020-12-07 15:48:26 -0500

    integrando interface

--- server/setting/init_db.ts
+++ server/setting/init_db.ts
@@ -1,11 +1,11 @@
 import { Pool, Client } from 'pg'
 
 const pool = new Pool({
-    user: 'sololectura',
+    user: 'normal_user',
     host: 'autogestion-dev.c66d2rypdwyt.us-east-2.rds.amazonaws.com',
     database: 'postgres',
-    password: 'nomas|puedo|leer',
-    port: 5432
+    password: 'ya|no|me|acuerdo',
+    port: 5432,
 })
 
 export const result = pool.query('SELECT * FROM pg_catalog.pg_user')
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
+  production: false,
   api: {
-    host: "https://5fc9a9223c1c220016440f27.mockapi.io/",
-    version: ""
+    host: "",
+    version: "api"
   }
 };
